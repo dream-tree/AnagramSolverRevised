021e35fa7bb66162d4d424f784691f0a412b014d
dream-tree <marcinklimek@yahoo.com>
2018-08-01 23:07:46 +0200

    adding unit test for AnagramEntryServiceImpl class

---
+++ src/test/java/com/marcin/anagramator/business/service/AnagramEntryServiceImplTest.java
@@ -0,0 +1,58 @@
+package com.marcin.anagramator.business.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnagramEntryServiceImplTest {
+
+	@Autowired
+	private AnagramEntryServiceImpl service;
+	
+	@Test
+	public void shouldReturnResultOfFirstIfConditionalInSplitAndValidateEntryMethod() {
+		Map<String, Set<String>> actual = service.splitAndValidateEntry("ab ba");
+		Map<String, Set<String>> expected = Map.of("error", new HashSet<>());
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void shouldReturnResultOfSecondIfConditionalInSplitAndValidateEntryMethod() {
+		Map<String, Set<String>> actual = service.splitAndValidateEntry("track");
+		Map<String, Set<String>> expected = Map.of("ackrt", new HashSet<String>() {{ add("track"); }});
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void shouldReturnResultOfElseConditionalInSplitAndValidateEntryMethod() {
+		Map<String, Set<String>> actual = service.splitAndValidateEntry("abcd dcba aaaa");
+		Set<String> testSet = new HashSet<>();
+		testSet.add("abcd");
+		testSet.add("dcba");
+		Map<String, Set<String>> expected = Map.of("error", testSet);
+		assertEquals(expected, actual);
+	}
+		
+	@Test
+	public void shouldReturnFinalResultOfIfElseConditionalInSplitAndValidateEntryMethod() {
+		Map<String, Set<String>> actual = service.splitAndValidateEntry("abcd dcba cdba");
+		Set<String> testSet = new HashSet<>();
+		testSet.add("abcd");
+		testSet.add("dcba");
+		testSet.add("cdba");
+		Map<String, Set<String>> expected = Map.of("abcd", testSet);
+		assertEquals(expected, actual);
+	}
+	
+	
+}
